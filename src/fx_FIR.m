8a1b0dd1c5e5ada7d1452ecedd3f0a60c543d925
Yaxuan Yao <120184643+yaoyaxuan@users.noreply.github.com>
2022-12-10 13:40:41 +0800

    Delete src directory

--- src/fx_FIR.m
+++
@@ -1,9 +0,0 @@
-function [ Y ] = fx_FIR( wc1 ,wc2 , signal)
-
-%===============FIRÉè¼ÆÂË²¨Æ÷===============  
-%Wn=[wc1*2/fs wc2*2/fs];
-global audioFs;
-b = fir1(100, [wc1*2/audioFs,wc2*2/audioFs]);
-Y = filter(b,1,signal);            
-
-
--- src/fx_FIRH.m
+++
@@ -1,6 +0,0 @@
-function [ Y ] = fx_FIRH( wc1 , signal)
-
-%===============FIRÉè¼ÆÂË²¨Æ÷===============  
-global audioFs;
-b = fir1(100, wc1*2/audioFs,'high');
-Y = filter(b,1,signal);         
